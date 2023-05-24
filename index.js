20796a05f30fbfe7cc8bace069f1d978eb51efab
micka-le-dev <mickael.petithory.pro@gmail.com>
2023-05-24 12:14:19 +0200

    rend le menu responsive

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("salut les gents !")
+const boutonMenu = document.querySelector('.nav-barre__toggle')
+const nav = document.querySelector('.nav-barre')
+const liensMenu = document.querySelectorAll('.nav-barre__lien-item')
+
+
+const toggleNav = () => {
+    boutonMenu.classList.toggle('nav-barre__toggle--active')
+    nav.classList.toggle('nav-barre--active')
+}
+boutonMenu.addEventListener('click', toggleNav)
+liensMenu.forEach(lien => lien.addEventListener('click', toggleNav))
